62afc197ef868a88e4d65c17cf0ca903cf2b288c
Errvin-Junius <2472024@maranatha.ac.id>
2025-06-09 20:31:34 +0700

    FIX EMAIL JS DAN BEBERAPA PERUBAHAN

--- script.js
+++
@@ -1,8 +0,0 @@
-function sendMail() {
-    let params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        pesan : document.getElementById("message").value,
-    }
-    emailjs.send("service_2s75ofg","template_zgmctem",params)
-}
